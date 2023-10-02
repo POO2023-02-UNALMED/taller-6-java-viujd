3465d7bb732f025bd9f5d387ebc0f92e566fb7da
Juan David Robayo <141962352+viujd@users.noreply.github.com>
2023-10-02 09:53:16 -0500

    Update Pais.java

--- src/test/java/vehiculos/Pais.java
+++ src/test/java/vehiculos/Pais.java
@@ -1,12 +1,16 @@
 package vehiculos;
+import java.util.ArrayList;
 
 public class Pais {
 	
 	private String nombre;
+	private static ArrayList<Pais> listado=new ArrayList<Pais>();
+	private int CantidadVentas;
 	
 	
 	public Pais(String nombre) {
 		this.nombre=nombre;
+		listado.add(this);
 	}
 	
 	public String getNombre() {
@@ -17,7 +21,29 @@ public class Pais {
 		this.nombre=nombre;
 	}
 	
+	public int getCantidadVentas() {
+		return CantidadVentas;
+	}
+	
+	public void setCantidadVentas(int CantidadVentas) {
+		this.CantidadVentas=CantidadVentas;
+	}
+	
 	public Pais paisMasVendedor() {
+		
+		int maxventas=0;
+		Pais paisMasVendedor=null;
+		
+		for (int i=0; i< Pais.listado.size(); i++) {
+			int CantidadVentas=Pais.getCantidadVentas();
+			
+			if (CantidadVentas>maxventas) {
+				maxventas=CantidadVentas;
+				paisMasVendedor= listado.get(i);
+			}
+		}
+		return paisMasVendedor;
+		
 		
 	}
 
