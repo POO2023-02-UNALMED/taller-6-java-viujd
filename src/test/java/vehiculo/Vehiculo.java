7b8a138cef9ba7cfe199762266691535dd83e844
Juan David Robayo <141962352+viujd@users.noreply.github.com>
2023-10-01 21:05:19 -0500

    Update Vehiculo.java

--- src/test/java/vehiculo/Vehiculo.java
+++ src/test/java/vehiculo/Vehiculo.java
@@ -1,5 +1,15 @@
 package vehiculo;
 
 public class Vehiculo {
+	
+	String placa;
+	int puertas;
+	int velocidadMaxima;
+	String nombre;
+	int precio;
+	int peso;
+	String traccion;
+	Fabricante fabricante;
+	int CantidadVehiculo=0;
 
 }
